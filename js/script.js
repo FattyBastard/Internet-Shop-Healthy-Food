6488d910d3a5aa0d11ec79271d35280afa36c96f
Nikita Vasiliev <mr.nikitausersan@mail.ru>
2022-07-21 18:12:28 +0300

    ability to save data from calc to localstorage was added

--- js/script.js
+++ js/script.js
@@ -389,9 +389,43 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const result = document.querySelector(".calculating__result span");
 
-    let sex = "female", 
-               height, weight, age, 
-               ratio = 1.375;
+    let sex, height, weight, age, ratio;
+    
+    if (localStorage.getItem("sex")){
+        sex = localStorage.getItem("sex");
+    }else{
+        sex = "female";
+        localStorage.setItem("sex", sex);
+    }
+
+    if (localStorage.getItem("ratio")){
+        sex = localStorage.getItem("ratio");
+    }else{
+        console.log("+");
+        ratio = 1.375;
+        localStorage.setItem("ratio", ratio);
+    }
+
+    function initBtnsStaticInfo(selector, activeClass){
+
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(element => {
+
+            element.classList.remove(activeClass);
+            if (localStorage.getItem("sex") === element.getAttribute("id")){
+                element.classList.add(activeClass);
+            }
+            if (localStorage.getItem("ratio") === element.getAttribute("ratio-data")){
+                element.classList.add(activeClass);
+            }
+        });
+
+    }
+
+    initBtnsStaticInfo("#gender div", "calculating__choose-item_active");
+    initBtnsStaticInfo(".calculating__choose_big div", "calculating__choose-item_active");
+    
 
     function calculate(){
 
@@ -414,15 +448,18 @@ window.addEventListener('DOMContentLoaded', () => {
     function getStaticInformation(parentSelector, activeClass){
 
         const elements = document.querySelectorAll(`${parentSelector} div`);
+        console.log(elements);
 
         elements.forEach(elem => {
             elem.addEventListener('click', (event) => {
 
                 if (event.target.getAttribute('ratio-data')){
                     ratio = +event.target.getAttribute('ratio-data');
+                    localStorage.setItem("ratio", +event.target.getAttribute('ratio-data'));
                 }
                 else{
                     sex = event.target.getAttribute("id");
+                    localStorage.setItem("sex", event.target.getAttribute("id"));
                 }
     
                 elements.forEach((elem) => {
